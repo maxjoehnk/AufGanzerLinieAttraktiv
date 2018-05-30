d1578bee69d9e4acfdd8624b36a28c9d1a5d6eac
Max Jöhnk <maxjoehnk@gmail.com>
2018-05-30 11:25:53 +0200

    [refactor] improve setup code and animate button state changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { renderWebGl } from './webgl-renderer';
 
-const JOLIE_URL = require('../assets/jolie.png');
-const SCARLET_URL = require('../assets/johannsonborder.png');
+const grils = [
+    { name: 'Angelina Jolie', url: require('../assets/jolie.png') },
+    { name: 'Scarlet Johannson', url: require('../assets/johannsonborder.png') }
+];
 
 async function fetchTweets() {
     const res = await fetch('/api/tweets');
@@ -17,65 +19,54 @@ function fetchImage(url) {
     });
 }
 
-function onOpenTweet(event) {
-    console.log(event);
-}
-
 async function setup() {
-    const jolie = await fetchImage(JOLIE_URL);
-    const renderJolie = render(jolie);
     const refreshBtn = document.querySelector('button');
-    refreshBtn.addEventListener('click', renderJolie);
+    const grilContainer = document.querySelector('.grils');
+    let selectedIndex = 0;
 
-    await renderJolie();
+    const grilBtns = grils.map((gril, i) => {
+        const btn = document.createElement('button');
+        btn.textContent = gril.name;
+        btn.addEventListener('click', () => {
+            selectedIndex = i;
+            render(gril.url);
+        });
+        grilContainer.appendChild(btn);
+        return btn;
+    });
+
+    const refreshBtns = () =>
+        grilBtns.forEach((btn, i) => i === selectedIndex ?
+            btn.classList.add('active') :
+            btn.classList.remove('active')
+        );
+    
+    const disable = btn => btn.setAttribute('disabled', '');
+    const enable = btn => btn.removeAttribute('disabled');
+
+    const render = async url => {
+        grilBtns.forEach(disable);
+        disable(refreshBtn);
+        refreshBtns();
+        try {
+            const img = await fetchImage(url);
+            const tweets = await fetchTweets();
+            const final = await renderWebGl(img, tweets);
+        }catch(e) {
+            console.error(e);
+        }
+        grilBtns.forEach(enable);
+        enable(refreshBtn);
+    };
+
+    const refresh = () => render(grils[selectedIndex].url);
+
+    refreshBtn.addEventListener('click', refresh);
+
+    await refresh();
     
     document.querySelector('.interactions').classList.add('show');
-    document.querySelector('.grils').classList.add('show');
-
-    const jolieBtn = document.querySelector('#jolie');
-    const scarletBtn = document.querySelector('#scarlet');
-
-    const scarlet = await fetchImage(SCARLET_URL);
-    const renderScarlet = render(scarlet);
-
-    const enable = () => {
-        refreshBtn.removeAttribute('disabled');
-        scarletBtn.removeAttribute('disabled');
-        jolieBtn.removeAttribute('disabled');
-    };
-
-    const disable = () => {
-        refreshBtn.setAttribute('disabled', '');
-        scarletBtn.setAttribute('disabled', '');
-        jolieBtn.setAttribute('disabled', '');
-    };
-
-    jolieBtn.addEventListener('click', async() => {
-        refreshBtn.addEventListener('click', renderJolie);
-        refreshBtn.removeEventListener('click', renderScarlet);
-        disable();
-        try {
-            await renderJolie();
-        }catch(e) {}
-        enable();
-    });
-
-    scarletBtn.addEventListener('click', async() => {
-        refreshBtn.addEventListener('click', renderScarlet);
-        refreshBtn.removeEventListener('click', renderJolie);
-        disable();
-        try {
-            await renderScarlet();
-        }catch(e) {}
-        enable();
-    });
-}
-
-function render(img) {
-    return async() => {
-        const tweets = await fetchTweets();
-        const final = await renderWebGl(img, tweets);
-    };
+    grilContainer.classList.add('show');
 }
 
 setup()
