5130e408dfe8daa37f8db574bdf272a3ccbb5c3b
Jonathan Kortjohann <jonathan.kortjohann@gmail.com>
2020-06-16 22:49:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const grils = [
 	{ name: 'Harvey Weinstein', url: require('../assets/weinstein.png') },
 	{ name: 'European Parliament', url: require('../assets/euparl.png') },
 	{ name: 'Alexa', url: require('../assets/alexa.png') },
-	{ name: 'Hollywood', url: require('../assets/hollywood.png') }/*,
+	{ name: 'Hollywood', url: require('../assets/hollywood.png') },/*
 	{ name: 'Carl Sargeant', url: require('../assets/carl_sargeant.png') },
 	{ name: 'Danny Masterson', url: require('../assets/danny_masterson.png')},
 	{ name: 'Dieter Wedel', url: require('../assets/dieter_wedel.png')},
 	{ name: 'James Toback', url: require('../assets/james_toback.png')},*/
-	{ name: 'Jörg Kachelmann', url: require('../assets/joerg_kachelmann.png')},
+	{ name: 'Jörg Kachelmann', url: require('../assets/joerg_kachelmann.png')}
 	//{ name: 'Ron Jeremy', url: require('../assets/ron-jeremy.png')}
 ];
 
